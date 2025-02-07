898bb67dcf7b5c6c43b7961bf1199e939f907d44
fuertesvic <fuertesvic@gmail.com>
2025-02-07 11:17:50 +0100

    add FPS limit and circleshape class (circleshape.py)

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets up the GUI window
-    
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():    # Make sure the 'x' works to quit the game
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(color='black')
-        pygame.display.flip()       # Refresh the screen
+        pygame.display.flip()                 # Refresh the screen 
+        clock.tick(60)             # Pause for 1/60th of a second and get how many time has passed
+        #dt = elapsed / 1000                   # Convert to seconds
 
 if __name__ == "__main__":
     main()
