8b3f5ada2706060e62aeba6eab2e5348700489d6
fuertesvic <fuertesvic@gmail.com>
2025-02-10 13:24:49 +0100

    add player.py, method draw() draws a triangle representing the player

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import * 
-
+from player import *
 
 def main():
 
@@ -12,12 +12,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets up the GUI window
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+
     while True:
         for event in pygame.event.get():    # Make sure the 'x' works to quit the game
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(color='black')
+        player.draw(screen)
         pygame.display.flip()                 # Refresh the screen 
         clock.tick(60)             # Pause for 1/60th of a second and get how many time has passed
         #dt = elapsed / 1000                   # Convert to seconds
