2c4421e2875b8d72b757319150570fb04065d1dc
fuertesvic <fuertesvic@gmail.com>
2025-03-05 09:47:19 +0100

    added player rotation and movement

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ from constants import *
 from player import *
 
 def main():
-
+    
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets up the GUI window
-    clock = pygame.time.Clock()
-    dt = 0
+    clock = pygame.time.Clock()                                     # Sets up the clock for the game loop    
+    dt = 0                              # This variable will be used to store time between user input
 
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)     # Create a "player" instance
 
 
     while True:
         for event in pygame.event.get():    # Make sure the 'x' works to quit the game
             if event.type == pygame.QUIT:
+                print("Exiting asteroids")
                 return
         
         screen.fill(color='black')
         player.draw(screen)
-        pygame.display.flip()                 # Refresh the screen 
-        clock.tick(60)             # Pause for 1/60th of a second and get how many time has passed
-        #dt = elapsed / 1000                   # Convert to seconds
+        player.update(dt)
+         
+        elapsed = clock.tick(60)             # Pause for 1/60th of a second and get how many time has passed
+        dt = elapsed / 1000                   # Convert to seconds
+        pygame.display.flip()                 # Refresh the screen
 
 if __name__ == "__main__":
     main()
